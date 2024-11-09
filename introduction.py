9f70fb1c123c9f43a803cff435f4d1b392ab596e
talish-gaurav <97028189+talish-gaurav@users.noreply.github.com>
2024-11-09 07:52:56 +0000

    Add files via upload

---
+++ introduction.py
@@ -0,0 +1,30 @@
+
+us_in='hveeho'
+
+if type(us_in) is int:
+
+    print("Your character is a number")
+
+else:
+
+    print("Your character is not a number")
+
+
+if type(us_in) is not int:
+
+    print("Your character is not a number")
+
+else:
+
+    print("Your character is a number")
+
+a=99
+b=25
+
+if a is b:
+
+    print("a and b are same ")
+
+else:
+
+    print("not same")
